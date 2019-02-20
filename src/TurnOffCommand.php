924f114f04dd99786e9be235b4f28a3da75e64b8
Jagepard <dankorot@gmail.com>
2019-02-20 13:21:24 +0300

    Update

--- src/TurnOffCommand.php
+++ src/TurnOffCommand.php
@@ -15,11 +15,5 @@ namespace Behavioral\Command;
  */
 class TurnOffCommand implements CommandInterface
 {
-    /**
-     * @param DeviceInterface $device
-     */
-    public function execute(DeviceInterface $device): void
-    {
-        $device->turnOff();
-    }
+    use CommandTrait;
 }
