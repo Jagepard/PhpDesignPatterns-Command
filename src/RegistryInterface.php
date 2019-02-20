26c957796573f93559e0226cf366a94ef117baee
Jagepard <dankorot@gmail.com>
2019-02-20 13:18:54 +0300

    Add file

---
+++ src/RegistryInterface.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * @author    : Korotkov Danila <dankorot@gmail.com>
+ * @license   https://mit-license.org/ MIT
+ */
+
+namespace Behavioral\Command;
+
+/**
+ * Interface RegistryInterface
+ * @package Behavioral\Command
+ */
+interface RegistryInterface
+{
+    /**
+     * @param CommandInterface $command
+     * @param TypeInterface    $type
+     */
+    public function setCommand(CommandInterface $command, TypeInterface $type): void;
+
+    /**
+     * @param TypeInterface $type
+     */
+    public function execute(TypeInterface $type): void;
+}
