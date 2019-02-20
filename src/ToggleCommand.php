5086bc9b8281395981f67fb14c204a1cf534f5f9
Jagepard <dankorot@gmail.com>
2019-02-20 13:19:26 +0300

    Update methods

--- src/ToggleCommand.php
+++ src/ToggleCommand.php
@@ -18,14 +18,31 @@ class ToggleCommand implements CommandInterface
     /**
      * @var int
      */
-    protected $toggle = 1;
+    private $toggle = 1;
 
     /**
      * @param DeviceInterface $device
      */
-    public function execute(DeviceInterface $device): void
+    public function execute(DeviceInterface $device, TypeInterface $type): void
     {
-        ($this->toggle % 2) ? $device->turnOn() : $device->turnOff();
-        $this->toggle++;
+        ($this->getToggle() % 2) ? $type->setName('on') : $type->setName('off');
+        $this->setToggle($this->getToggle() + 1);
+        $device->execute($type);
+    }
+
+    /**
+     * @return int
+     */
+    public function getToggle(): int
+    {
+        return $this->toggle;
+    }
+
+    /**
+     * @param int $toggle
+     */
+    public function setToggle(int $toggle): void
+    {
+        $this->toggle = $toggle;
     }
 }
