13eb212ce980a003c68cfcba4a86a0753d1834f2
Jagepard <dankorot@gmail.com>
2019-02-20 13:17:18 +0300

    Add file

---
+++ src/CommandType.php
@@ -0,0 +1,47 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * @author    : Korotkov Danila <dankorot@gmail.com>
+ * @license   https://mit-license.org/ MIT
+ */
+
+namespace Behavioral\Command;
+
+/**
+ * Class CommandType
+ * @package Behavioral\Command
+ */
+class CommandType implements TypeInterface
+{
+    /**
+     * @var string
+     */
+    private $name;
+
+    /**
+     * CommandType constructor.
+     * @param $name
+     */
+    public function __construct(string $name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     */
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+}
