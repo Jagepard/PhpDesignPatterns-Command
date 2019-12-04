0c6cad40ec4a61510c13fb20cf9e7990e8b63bd4
Jagepard <dankorot@gmail.com>
2019-12-04 19:13:49 +0300

    extend Lamp

--- src/Lamp.php
+++ src/Lamp.php
@@ -3,7 +3,7 @@
 declare(strict_types=1);
 
 /**
- * @author  : Korotkov Danila <dankorot@gmail.com>
+ * @author  : Jagepard <jagepard@yandex.ru>
  * @license https://mit-license.org/ MIT
  */
 
@@ -12,10 +12,32 @@ namespace Behavioral\Command;
 class Lamp implements DeviceInterface
 {
     /**
+     * @var array
+     */
+    private $commandsRegistry = [];
+
+    /**
      * @param string $type
      */
     public function execute(string $type): void
     {
-        printf("The Light turns %s \n", $type);
+        if (!array_key_exists($type, $this->commandsRegistry)) {
+            throw new \InvalidArgumentException('Type does not exist in the Registry');
+        }
+
+        $this->commandsRegistry[$type]->execute();
+    }
+
+    /**
+     * @param string           $type
+     * @param CommandInterface $command
+     */
+    public function setCommand(string $type, CommandInterface $command): void
+    {
+        if (array_key_exists($type, $this->commandsRegistry)) {
+            throw new \InvalidArgumentException('Command already exists');
+        }
+
+        $this->commandsRegistry[$type] = $command;
     }
 }
