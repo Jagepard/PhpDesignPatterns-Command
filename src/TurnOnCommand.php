c642c5a6e7e85a7f45f540f3aaa79e490c87db0e
Jagepard <dankorot@gmail.com>
2019-12-05 12:19:31 +0300

    remove docblocks

--- src/TurnOnCommand.php
+++ src/TurnOnCommand.php
@@ -9,10 +9,6 @@ declare(strict_types=1);
 
 namespace Behavioral\Command;
 
-/**
- * Class TurnOnCommand
- * @package Behavioral\Command
- */
 class TurnOnCommand implements CommandInterface
 {
     public function execute(): void
